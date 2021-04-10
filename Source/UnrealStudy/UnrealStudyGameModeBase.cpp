755f9e051559d5701e28e3ea5ac235c3b8127caf
Kijung Kim <rlwjd1504@gmail.com>
2021-04-10 21:54:18 +0900

    UMG and User Interface

--- Source/UnrealStudy/UnrealStudyGameModeBase.cpp
+++ Source/UnrealStudy/UnrealStudyGameModeBase.cpp
@@ -2,4 +2,28 @@
 
 
 #include "UnrealStudyGameModeBase.h"
+#include <Blueprint/UserWidget.h>
+
+void AUnrealStudyGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void AUnrealStudyGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
 
